d845b2393225c01a7ab8f636c4250b094c3810a4
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2022-07-12 10:32:20 +0530

    Get Connection

--- src/controller/ManageCustomerFormController.java
+++ src/controller/ManageCustomerFormController.java
@@ -1,2 +1,37 @@
-package controller;public class ManageCustomerFormController {
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ManageCustomerFormController {
+    public TextField txtStudentId;
+    public TextField txtStudentName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtNic;
+    public TextField txtSearch;
+    public TableView tblStudent;
+    public TableColumn colStudentId;
+    public TableColumn colStudentName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+
+
+
+    public void btnSaveStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddNewStudentOnAction(ActionEvent actionEvent) {
+    }
 }
